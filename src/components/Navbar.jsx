b1922ff4ab4ad9e42e43a35de0cc60ba4b6b340b
Rioba Ian <riobafelix@gmail.com>
2023-04-13 11:28:11 +0300

    style the header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import { Link } from "gatsby";
 import React from "react";
+import "../css/navbar.css";
 import links from "../constants/links";
+import logo from "../images/travel-leisure-seeklogo.com.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
   return (
     <nav>
       <div className="navbar">
         <div className="nav-logo">
-          <img src="" alt="" />
-          <button></button>
+          <img src={logo} alt="" />
         </div>
+        <button>
+          <GiHamburgerMenu />
+        </button>
 
         <ul className="nav-links">
           {links.map((item, index) => {
